6c3f77efffe842fc8156f31efff8cc57e93b0c1f
karelalekoy <karel.alekoy@gmail.com>
2024-06-06 23:58:01 -0400

    aaa

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        
+
+        Running run = new Running("June 5th, 2024", 15, 1.2);
+        StationaryBike bike = new StationaryBike("June 1th, 2024", 22, 10);
+        Swimming swim = new Swimming("June 1th, 2024", 22, 120);
+
+        Console.WriteLine("Exercise activites for June:");
+        Console.WriteLine(run.GetSummary());
+        Console.WriteLine(bike.GetSummary());
+        Console.WriteLine(swim.GetSummary());
     }
 }
